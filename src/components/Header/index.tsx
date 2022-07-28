bcd1d202a8208119c63b840ffd6b290a7ec8a401
K4geNo <micaraujo71@gmail.com>
2022-07-27 23:49:17 -0300

    Feat: created Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,19 @@
+import { Flex, Image } from "@chakra-ui/react"
 import React from "react"
 
+import Logo from "../../assets/Logo.svg"
+
 export function Header() {
-    return <div>Header</div>
+    return (
+        <Flex
+            w={"full"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            pt={"4.5rem"}
+            pb={"5rem"}
+            bg={"gray.700"}
+        >
+            <Image src={Logo} alt="Logo" width="7.875rem" height="3rem" />
+        </Flex>
+    )
 }
